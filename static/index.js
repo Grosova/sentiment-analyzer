399e35c691bf2fbedd7c34540f4aadadec991e15
Natalia Svergun <Natalia.Svergun@dataart.com>
2019-12-23 13:53:25 +0200

    updated

--- static/index.js
+++ static/index.js
@@ -1,30 +1,31 @@
-$(function() {
-    $('form').submit(function(e){
-        e.preventDefault(); 
-        var form = $(this);
-        var data = {            
-            'feedback': $("textarea[name='feedback']", form).val()
-        };
+$(function () {
+    $('form').submit(function (e) {
+        e.preventDefault();
 
+        var form = $(this);
+        var feedback = $("textarea[name='feedback']", form).val();
+        
+        if(!feedback) return;
+        
         $.ajax({
-            url: form.attr('action'),          
-            data: data,
+            url: form.attr('action'),
+            data: { "feedback": feedback },
             type: 'POST',
             dataType: 'json',
-            success: function(response) {
-                if(response.res == 1 ){
-                    $(".js-thank-you-positive", form).show();
-                    $(".js-thank-you-negative", form).hide();
+            success: function (response) {
+                if (response.res == 1) {
+                    $(".js-thank-you-positive", form).removeClass('hidden');
+                    $(".js-thank-you-negative", form).addClass('hidden');
                 }
-                else{
-                    $(".js-thank-you-positive", form).hide();
-                    $(".js-thank-you-negative", form).show();
+                else {
+                    $(".js-thank-you-positive", form).addClass('hidden');
+                    $(".js-thank-you-negative", form).removeClass('hidden');
                 };
                 console.log(response);
             },
-            error: function(error) {
+            error: function (error) {
                 console.log(error);
             }
         });
-   });
+    });
 });
